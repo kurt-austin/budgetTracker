e395aaa4bfdde046041a7052bcc3af3fad680a78
Kurt Austin <kurt.austin@gmail.com>
2020-09-15 23:29:09 -0500

    modified server.js connect string

--- server.js
+++ server.js
@@ -3,27 +3,33 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// const PORT = 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// routes
+app.use(require("./routes/api.js"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
-});
 
-// routes
-app.use(require("./routes/api.js"));
+}
+);
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
